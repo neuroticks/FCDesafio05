d2924b4d0e1620b6adb23f61629af4be0bf62408
Rodrigo Ferraro <neuroticks@gmail.com>
2024-09-28 18:42:58 +0000

    1. criação do UseCase CheckStock 2. atualização da Spec para o UseCase CheckStock, importando o UseCase criado.

--- src/modules/product-adm/usecase/check-stock/check-stock.usecase.spec.ts
+++ src/modules/product-adm/usecase/check-stock/check-stock.usecase.spec.ts
@@ -1,5 +1,6 @@
 import Id from "../../../@shared/domain/value-object/id.value-object";
 import Product from "../../domain/product.entity";
+import CheckStockUseCase from "./check-stock.usecase";
 
 const product = new Product({
   id: new Id("1"),
