4a0523da98a37c6cb09d40709c937e9c09d4fc62
Rodrigo Ferraro <neuroticks@gmail.com>
2024-09-28 18:54:17 +0000

    atualização da Factory com o novo UseCase CheckStock.

--- src/modules/product-adm/factory/facade.factory.ts
+++ src/modules/product-adm/factory/facade.factory.ts
@@ -1,12 +1,19 @@
 import ProductAdmFacade from "../facade/product-adm.facade";
 import ProductRepository from "../repository/product.repository";
 import AddProductUseCase from "../usecase/add-product/add-product.usecase";
+import CheckStockUseCase from "../usecase/check-stock/check-stock.usecase";
 
-export default class ProductAdmFacadeFactory{
-    static create(){
+export default class ProductAdmFacadeFactory {
+    static create() {
         const l_productRepository = new ProductRepository
+
         const l_addProductUseCase = new AddProductUseCase(l_productRepository)
-        const l_productFacade = new ProductAdmFacade({addUseCase: l_addProductUseCase, stockUseCase: undefined})
+        const l_checkStocktUseCase = new CheckStockUseCase(l_productRepository)
+
+        const l_productFacade = new ProductAdmFacade({
+            addUseCase: l_addProductUseCase,
+            stockUseCase: l_checkStocktUseCase
+        })
 
         return l_productFacade
     }
